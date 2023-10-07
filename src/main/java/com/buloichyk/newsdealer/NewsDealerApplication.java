b2cddfec6c90fb027af87a35b0c5ee90ebe499c8
kiril <89221217@student.upr.si>
2023-10-07 18:47:35 +0200

    Create git repository for the project

---
+++ src/main/java/com/buloichyk/newsdealer/NewsDealerApplication.java
@@ -0,0 +1,13 @@
+package com.buloichyk.newsdealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsDealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsDealerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buloichyk/newsdealer/NewsDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buloichyk.newsdealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsDealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
